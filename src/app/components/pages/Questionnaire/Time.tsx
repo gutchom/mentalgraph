8eedf1c549480f5bde933c819d998552183959f8
gutchom <yskz.tg@gmail.com>
2020-05-10 12:08:36 +0900

    Change input sleeping times to select tag

--- src/app/components/pages/Questionnaire/Time.tsx
+++ src/app/components/pages/Questionnaire/Time.tsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export type TimeProps = {
   title: string;
   defaultHour: string;
 }
 
+function range(length: number): number[] {
+  return [...Array(length)].map((_, n) => n)
+}
+
 export const Time: React.FC<TimeProps> = props => {
+  const [hour, setHour] = useState('')
+  const [minute, setMinute] = useState('')
+
   return (
     <fieldset className="question time">
       <legend>{props.title}</legend>
       <div className="container">
-        <input
-          type="number"
-          placeholder={props.defaultHour ?? '8'}/>
+        <select dir="rtl" value={hour} onChange={e => setHour(e.target.value)}>
+          {['', ...range(24)].map(hour => (<option key={hour} value={hour}>{hour}</option>))}
+        </select>
         <span>時</span>
 
-        <input
-          type="number"
-          placeholder="30"/>
+        <select dir="rtl" value={minute} onChange={e => setMinute(e.target.value)}>
+          {['', ...range(60)].map(minute => (<option key={minute} value={minute}>{minute}</option>))}
+        </select>
         <span>分</span>
       </div>
     </fieldset>
