d60cb1f87c72d1271f8acdfdb29f4be1b3530106
gutchom <yskz.tg@gmail.com>
2020-05-13 20:01:08 +0900

    Implement scroll position reset

--- src/app/components/layouts/BasicLayout.tsx
+++ src/app/components/layouts/BasicLayout.tsx
@@ -1,12 +1,19 @@
+import React, { ReactElement, useEffect } from 'react'
+import { useLocation } from "react-router-dom";
 import { Header, HeaderProps } from 'app/components/modules/Header'
 import { Navigation } from 'app/components/modules/Navigation'
-import React, { ReactElement } from 'react'
 
 export type BasicLayoutProps = HeaderProps & {
   children: ReactElement
 }
 
 export const BasicLayout: React.FC<BasicLayoutProps> = props => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return (
     <>
       <Header title={props.title}/>
